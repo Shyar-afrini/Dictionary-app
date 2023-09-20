f50bdde0badb50a9dea0c3162883283b25b5c088
Shyar-Afrini <shyarafrin2@gmail.com>
2023-09-20 22:44:22 +0300

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,25 +1,27 @@
-import React, { useState, useRef } from 'react';
-import Input from './Input';
-import FetchData from './FetchData';
+import React, { useState } from "react";
+import Input from "./Input";
+import FetchData from "./FetchData";
 
 const Navbar = () => {
   const [theme, setTheme] = useState(false);
 
   const darkModeToggle = () => {
-    setTheme(!theme);
+    setTheme((prev) => !prev);
   };
 
-  
   return (
-    <div id='division'
+    <div
+      id="division"
       className={`lg:px-12 relative w-screen h-fit transition-all duration-500 ${
-        theme ? 'bg-white text-[#303030]' : 'bg-[#0e0e10] text-white'
+        theme ? "bg-white text-[#303030]" : "bg-[#0e0e10] text-white"
       }`}
     >
       <div className="w-full h-[2rem] font-cursive p-6 pt-8 md:p-10 flex justify-between">
         <h1
           className={`text-2xl font-bold italic cursive h-7 ${
-            theme ? 'drop-shadow-lg shadow-black' : 'drop-shadow-lg shadow-white'
+            theme
+              ? "drop-shadow-lg shadow-black"
+              : "drop-shadow-lg shadow-white"
           }`}
         >
           Your AB&amp;C of the internet
@@ -30,7 +32,7 @@ const Navbar = () => {
         </div>
       </div>
       <Input theme={theme} />
-      <div className='h-screen'>
+      <div className="h-screen">
         <FetchData theme={theme} />
       </div>
     </div>
